a01bbac7140590b16952c2ce16dd6c443bd12773
Federico Guerinoni <guerinoni@micro-systems.it>
2019-01-05 21:10:58 +0100

    change main functions to allow convert ts in csv and vice versa

--- main.cpp
+++ main.cpp
@@ -5,20 +5,23 @@
 int main(int argc,  char **argv)
 {
     if (argc <= 1) {
-        std::cerr << "no arguments" << std::endl;
-        return 0;
+        throw std::logic_error("no argumets");
     }
 
     std::string file(argv[1]);
-    if (file.find(".ts") == std::string::npos) {
-        std::cerr << "file not supported" << std::endl;
-        return 0;
+    if (file.find(".ts") != std::string::npos &&
+        file.find(".csv") != std::string::npos) {
+        throw std::logic_error("file not supported");
     }
 
+    std::cout << "converting file " << file;
+
     if (file.find(".ts") != std::string::npos) {
-        std::cout << "converting file " << file << " in csv" << std::endl;
+        std::cout << " in csv" << std::endl;
         Ts2Csv ts2csv(std::move(file));
         ts2csv.convert();
+    } else {
+        std::cout << " in ts" << std::endl;
     }
 
     std::cout << "terminated" << std::endl;
