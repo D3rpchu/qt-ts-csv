d58cb16ed4e2ca426aeaa3372b7374a006635711
Oisin Hartnett <41676096+D3rpchu@users.noreply.github.com>
2019-02-22 08:36:05 +0100

    Resolved bug that make the application crash if no files are selected

--- src/Converter.cpp
+++ src/Converter.cpp
@@ -4,20 +4,25 @@
 
 QString Converter::convert(QString source, QString dest) const
 {
+    if (source.isEmpty() || dest.isEmpty()) {
+        return "Empty arguments";
+    }
+
+    std::string sep = "///";
     std::string input = source.toStdString();
-    input = input.substr(input.find("C"));
+    input = input.substr(input.find(sep) + sep.length());
     std::string output = dest.toStdString();
-    output = output.substr(output.find("C"));
+    output = output.substr(output.find(sep) + sep.length());
 
     if (input.find(".ts") != std::string::npos &&
         input.find(".csv") != std::string::npos &&
         input.find(".xlsx") != std::string::npos) {
-        return "Estensione non valida";
+        return "Not valid extention";
     }
 
     if (input.find(".xlsx") != std::string::npos &&
         output.find(".ts") == std::string::npos) {
-        return "Conversione non valida";
+        return "Not valid conversion";
     }
     if (input.find(".ts") != std::string::npos) {
         if (output.find(".csv") != std::string::npos) {
@@ -25,20 +30,21 @@ QString Converter::convert(QString source, QString dest) const
         } else if (output.find(".xlsx") != std::string::npos) {
             Ts2Xlsx().convert(std::move(input), std::move(output));
         } else {
-            return "Conversione non valida";
+            return "Not valid conversion";
         }
     } else if (input.find(".csv") != std::string::npos){
         if (output.find(".ts") != std::string::npos) {
             Csv2Ts().convert(std::move(input), std::move(output));
         } else {
-            return "Conversione non valida";
+            return "Not valid conversion";
         }
     } else {
         if (output.find(".ts") != std::string::npos) {
             Xlsx2Ts().convert(std::move(input), std::move(output));
         } else {
-            return "Conversione non valida";
+            return "Not valid conversion";
         }
     }
-    return "Conversione eseguita";
+    return "Conversion terminated";
 }
+
